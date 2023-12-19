25448f47b3d2232f4500ad6c67b58a73e9a8be52
Mehmood Hassan Shah <mehmoodhassan595@yahoo.com>
2023-12-19 14:07:23 +0500

    Vendor registration completed

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,10 @@ from django.contrib import messages
 from django.http import HttpRequest
 from django.shortcuts import redirect, render
 
+from vendor.forms import VendorForm
+
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
 
 logger = logging.getLogger("custom_logger")
 
@@ -31,3 +33,39 @@ def registerUser(request: HttpRequest) -> render or redirect:
         "form": form,
     }
     return render(request, "accounts/registerUser.html", context)
+
+
+# "accounts/registerVendor"
+def registerVendor(request: HttpRequest) -> render or redirect:
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid():
+            password = form.cleaned_data["password"]
+            user = form.save(commit=False)
+            user.role = User.VENDOR
+            user.set_password(password)
+            user.save()
+
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+
+            messages.success(
+                request,
+                "Your request has been submitted successfully. Please wait for the approval",
+            )
+            return redirect("home")
+        else:
+            messages.error(request, "Failed to register business")
+            logger.error(form.errors)
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+    context = {
+        "v_form": v_form,
+        "form": form,
+    }
+    return render(request, "accounts/registerVendor.html", context)
